4ffe7d5c64dcc90fff8de3d6329ccadac470da3b
Diego Canteros <123302060+turbina440@users.noreply.github.com>
2023-03-04 23:50:30 -0300

    Add files via upload
    es la correccion acorde a la devolución

---
+++ CV.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('mostrar-cv').addEventListener('click',function(){
+document.getElementById('main').style.display='block';
+document.getElementById('ocultar-footer').style.display='block'})
+
+
+document.getElementById('ocultar-cv').addEventListener('click',function(){
+document.getElementById('main').style.display='none';
+});
---
+++ ubicacion.js
@@ -0,0 +1,8 @@
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.68818 , -58.56000),
+    zoom: 15
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
